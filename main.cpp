7a1bb8d2ebb228af49b52a9e20910361efe7d47b
Notraly <fernandez.coralie2001@gmail.com>
2018-09-05 20:20:26 +0200

    feat(led): create led component  FEAT-02

--- main.cpp
+++ main.cpp
@@ -2,18 +2,24 @@
 #include "Util.h"
 #include "Box.h"
 
+
 Box *box;
 
 
 void setup() {
     Serial.begin(9600);
-    Sensor *slotSensor = new Sensor(2,200);
-    Sensor *doorSensor = new Sensor(4,200);
 
-    box = new Box(slotSensor, doorSensor);
+    Sensor *slotSensor = new Sensor(2, 200);
+    Sensor *doorSensor = new Sensor(4, 200);
+    Led *letterLed = new Led(3, 0);
 
-    pinMode(2,INPUT);
-    pinMode(4,INPUT);
+    box = new Box(slotSensor, doorSensor, letterLed);
+
+    pinMode(2, INPUT);
+    pinMode(4, INPUT);
+    pinMode(3, OUTPUT);
+
+    letterLed->init();
 
 }
 
