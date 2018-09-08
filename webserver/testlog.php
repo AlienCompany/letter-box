4c3a70bb5e3a28dac8c3cdeaf5f50c0dc7f0f987
Notraly <fernandez.coralie2001@gmail.com>
2018-09-08 14:14:51 +0200

    feat(web-log): creat log function FEAT-06

--- webserver/testlog.php
+++ webserver/testlog.php
@@ -5,25 +5,10 @@
  * Date: 08/09/2018
  * Time: 10:37
  */
+require "letterLog.php";
 
-$today = date("d-m-Y");
-//echo $today;
-$fileName = 'log-'.$today.'.txt';
+$ip = getClientIp();
 
-echo $fileName; // log-08-09-2018.txt
-
-$monfichier = fopen('logs/'.$fileName ,'a+');
-
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-fputs($monfichier, '['.date("Y-m-d H:i:s").'] clientIp:'.$ip.' url: /testlog.php');
-
-fclose($monfichier);
-
+letterLog('clientIp:'.$ip." url: /testlog.php");
+letterLog('clientIp:'.$ip." url: /log.php");
 ?>
